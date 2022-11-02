f1987f42452c5e66f5e145bbbfec135a57cc1fa2
5erious5am <87398589+5erious5am@users.noreply.github.com>
2022-11-01 19:56:42 -0500

    Add files via upload

---
+++ room.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    var discover = $('#discover');
+    var available = $('#available');
+    var manufacture = $('#manufacture');
+    var top = $('#top');
+
+    $('.right').on('click', function () {
+
+        
+    });
+
+});
